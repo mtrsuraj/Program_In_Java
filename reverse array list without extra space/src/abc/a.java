46846b72275e127ad72cfa9cc49c473e3f7381e3
mtrsuraj <surajprintf33@gmail.com>
2023-01-29 15:06:04 +0530

    reverse array list in java

---
+++ reverse array list without extra space/src/abc/a.java
@@ -0,0 +1,35 @@
+// 
+
+package abc;
+
+import java.util.Scanner;
+
+public class a {
+	public static void main(String[] args) {
+		int temp=0, i, j;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+		int n = sc.nextInt();
+		int [] arr = new int[n];
+		for( i=0; i<arr.length; i++) {
+			System.out.println("enter array element");
+			arr[i] = sc.nextInt();
+			
+		}
+		// reverse array element
+		i=0;
+		j = arr.length-1;
+		while(i<j) {
+			temp= arr[i];
+			arr[i]= arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+		for( i=0; i<arr.length; i++) {
+			System.out.println(arr[i] + " ");
+		}
+			
+	}
+
+}
