ca332ec3be2a9b9956a0efd5636a553e18708a06
mtrsuraj <surajprintf33@gmail.com>
2023-01-29 22:08:25 +0530

    Auto-payment

---
+++ add-to-cart-master/src/app/component/products/products.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
+
+declare var window :any;
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  public productList : any ;
+  public filterCategory : any
+  searchKey:string ="";
+  formModal:any;
+  constructor(private api : ApiService, private cartService : CartService) { }
+
+  ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById("exampleModalCenter")
+    );
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+      this.filterCategory = res;
+      this.productList.forEach((a:any) => {
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category ="fashion"
+        }
+        Object.assign(a,{quantity:1,total:a.price});
+      });
+      console.log(this.productList)
+    });
+
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
+    })
+  }
+  addtocart(item: any){
+    this.formModal.show();
+    this.cartService.addtoCart(item);
+  
+  }
+  filter(category:string){
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category==''){
+        return a;
+      }
+    })
+  }
+ 
+ doSomething() {
+    this.formModal.hide();
+  }
+
+}
